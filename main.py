d8de222a2609ccf73237e1f2dc32dc33416f22de
rishibrainerhub <108719630+rishibrainerhub@users.noreply.github.com>
2022-11-02 11:18:00 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,22 @@
+import streamlit as st
+from data_preprosesing import custom_input_prediction
+
+st.header("Type of CyberBully")
+
+st.image("rename.png")
+
+text = st.text_area("Enter tweet here", placeholder="Tweet")
+
+b = st.button("Check")
+
+if b:
+    if text == "":
+        st.info("'LMAO' enter some text dwag")
+    else:
+        output = custom_input_prediction(text)
+        if output == "Not Cyberbullying":
+            st.success(output)
+        else:
+            st.warning(output)
+
+
